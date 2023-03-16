47ebba771bf1d76ef50fefa9916580e4456c8b13
Duncan <cheplenginduncan@gmail.com>
2023-03-16 21:11:13 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide() {
+    const someDivision = 2000 / 100;
+    return someDivision;
+}
+
+const square = (param = 2) => {
+    return param * param;
+
+}
+
+const add = (a = 3, b = 4) => {
+    return a + b;
+
+}
